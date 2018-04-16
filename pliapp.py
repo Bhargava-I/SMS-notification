fc76d1de041116a9a037c26cb5ea50e4ef44c710
Bhargava-I <38428230+Bhargava-I@users.noreply.github.com>
2018-04-16 18:56:45 +0530

    Add files via upload

---
+++ pliapp.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, request, make_response, Response, Flask, flash, redirect, render_template, request, session, abort
+import plivo
+app = Flask(__name__, static_url_path='')
+
+
+@app.route('/send/')
+def outbound_sms():
+    client = plivo.RestClient(auth_id = 'MAZTYXNTQYMTBIYTLHZD', auth_token = 'MzdiM2QxMTU2NzhlZjBiY2RmYThjZWY0ZjkyYmEx')
+
+    try:
+        response = client.messages.create(
+            src = '11111111', # Sender's phone number with country code
+            dst = '919538183813', # Receiver's phone Number with country code
+            text = 'You have a call coming your way',
+        )
+    #response = p.send_message(params)
+    # Prints the complete response
+        return str(response)
+    except plivo.exceptions.PlivoRestError as e:
+        print(e)
+
+if __name__ == "__main__":
+    app.run(debug=True)
